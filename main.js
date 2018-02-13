12589e48a73a6044c95ce154991f65997c99816f
Luc Hutton <luc.j.hutton@gmail.com>
2018-02-13 13:55:40 +0000

    zenmode refactor

--- main.js
+++ main.js
@@ -40,57 +40,27 @@ function toBlack(){
 }
 
 function zenOn() {
-  let p0 = document.getElementsByTagName('p')[0];
-  let p1 = document.getElementsByTagName('p')[1];
-  let p2 = document.getElementsByTagName('p')[2];
-  let p3 = document.getElementsByTagName('p')[3];
-  let p4 = document.getElementsByTagName('p')[4];
-  let p5 = document.getElementsByTagName('p')[5];
-  let p6 = document.getElementsByTagName('p')[6];
-  let p7 = document.getElementsByTagName('p')[7];
-  let p8 = document.getElementsByTagName('p')[8];
-  let p9 = document.getElementsByTagName('p')[9];
-
-  p1.setAttribute("style","display:" + "none");
-  p2.setAttribute("style","display:" + "none");
-  p3.setAttribute("style","display:" + "none");
-  p4.setAttribute("style","display:" + "none");
-  p5.setAttribute("style","display:" + "none");
-  p6.setAttribute("style","display:" + "none");
-
-  if (theme === "light"){
-    p0.setAttribute("style","color:" + "#dddddd");
-    p7.setAttribute("style","color:" + "#dddddd");
-    p8.setAttribute("style","color:" + "#dddddd");
-    p9.setAttribute("style","color:" + "#dddddd");
-  } else if (theme === "dark") {
-    p0.setAttribute("style","color:" + "#5b6772");
-    p7.setAttribute("style","color:" + "#5b6772");
-    p8.setAttribute("style","color:" + "#5b6772");
-    p9.setAttribute("style","color:" + "#5b6772");
+  let ps = document.getElementsByTagName('p');
+  for(let i = 1; i < ps.length; i++){
+    if (i === 3){
+      continue;
+    } else{
+    let psi = ps[i];
+    psi.setAttribute("style", "display:" + "none")
+    }
   }
+  
+  let toggle = document.getElementById("zen-toggle");
+  toggle.setAttribute("onclick", "zenOff()");
 }
 
 function zenOff(){
-  let p0 = document.getElementsByTagName('p')[0];
-  let p1 = document.getElementsByTagName('p')[1];
-  let p2 = document.getElementsByTagName('p')[2];
-  let p3 = document.getElementsByTagName('p')[3];
-  let p4 = document.getElementsByTagName('p')[4];
-  let p5 = document.getElementsByTagName('p')[5];
-  let p6 = document.getElementsByTagName('p')[6];
-  let p7 = document.getElementsByTagName('p')[7];
-  let p8 = document.getElementsByTagName('p')[8];
-  let  p9 = document.getElementsByTagName('p')[9];
+  let ps = document.getElementsByTagName('p');
+  for (let i = 0; i < ps.length; i++) {
+    let psi = ps[i];
+    psi.removeAttribute("style");
+  }
 
-  p0.removeAttribute("style");
-  p1.removeAttribute("style");
-  p2.removeAttribute("style");
-  p3.removeAttribute("style");
-  p4.removeAttribute("style");
-  p5.removeAttribute("style");
-  p6.removeAttribute("style");
-  p7.removeAttribute("style");
-  p8.removeAttribute("style");
-  p9.removeAttribute("style");
+  let toggle = document.getElementById("zen-toggle");
+  toggle.setAttribute("onclick", "zenOn()");
 }
